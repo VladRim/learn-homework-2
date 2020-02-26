5e6eef795b9ab63f27777addef460934b7b7bf58
admsys <vlad_rym@mail.ru>
2020-02-26 11:32:31 +0300

    added file dates.py who printed dates today, tomorrow yesterday and date last month on thia date

---
+++ dates.py
@@ -0,0 +1,30 @@
+from datetime import datetime,timedelta
+import locale
+
+
+
+def print_date(name_day, date_print):
+    str_date = date_print.strftime('%A %d %B %Y')
+    print(f'{name_day}{str_date}')
+
+
+def date_time():
+    locale.setlocale(locale.LC_ALL, "russian")
+    date_now = datetime.now()
+    
+    print_date('Сегодня ', date_now)
+  
+    delta = timedelta(days=1)
+    tomorrow = date_now + delta
+    print_date('Завтра будет ', tomorrow)
+ 
+    yesterday = date_now - delta
+    print_date('Вчера был ', yesterday)
+
+    delta_month = timedelta(days=31)
+    last_month = date_now - delta_month
+    print_date('Месяц назад был день ', last_month)
+
+
+if __name__ == "__main__":
+    date_time()
