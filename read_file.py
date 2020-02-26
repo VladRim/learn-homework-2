7bb877754f84ca05e542aeb0c17136973cde4d2c
admsys <vlad_rym@mail.ru>
2020-02-26 17:15:42 +0300

    fix unnessary code

--- read_file.py
+++ read_file.py
@@ -1,8 +1,3 @@
-def remove_dot(substring):
-	index = substring.find(".")
-	substring[index] = "!"
-	return index
-
 def read_file():
 	with open('referat.txt', 'r', encoding = "utf-8") as file:
 		content = file.read()
@@ -12,7 +7,7 @@ def read_file():
 	content_new = content.replace('.', '!')
 	print(content_new)
 	with open('referat2.txt', 'w', encoding = "utf-8") as file:
-		file.write(content_new)
+		file.write(content_new = )
 
 if __name__ == "__main__":
 	read_file()
