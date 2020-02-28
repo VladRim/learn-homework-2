47f39eb584e201e3388bc48d2fa2d46810c288c0
admsys <vlad_rym@mail.ru>
2020-02-28 14:05:12 +0300

    delete *.txt, *.csv files from repo

--- read_file.py
+++ read_file.py
@@ -1,13 +1,14 @@
 def read_file():
-	with open('referat.txt', 'r', encoding = "utf-8") as file:
-		content = file.read()
-	print(f'Длина строки: {len(content)} символов')
-	list_content = content.split(' ')
-	print(f'Файл содержит {len(list_content)} слов')
-	content_new = content.replace('.', '!')
-	print(content_new)
-	with open('referat2.txt', 'w', encoding = "utf-8") as file:
-		file.write(content_new = )
+    with open('referat.txt', 'r', encoding="utf-8") as file:
+        content = file.read()
+    print(f'Длина строки: {len(content)} символов')
+    list_content = content.split(' ')
+    print(f'Файл содержит {len(list_content)} слов')
+    content_new = content.replace('.', '!')
+    print(content_new)
+    with open('referat2.txt', 'w', encoding="utf-8") as file:
+        file.write(content_new)
+
 
 if __name__ == "__main__":
-	read_file()
+    read_file()
